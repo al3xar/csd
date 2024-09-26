fe55076a6d8acbd317bb4e69251a476ab446e442
Alex Arenas <106154424+al3xar@users.noreply.github.com>
2024-09-26 18:40:43 +0000

    feat: funcionalizado

--- rainbow/rb.py
+++ rainbow/rb.py
@@ -8,15 +8,9 @@ def reverse(hash: str) -> str:
     length = 6 + (first_5_bits % 14)
     remaining_bits = int(hash_value, 2)
     password = str(remaining_bits).zfill(10)[:length]
-    
-    # print(f"Hash: {hash}")
-    # print(f"First 5 bits: {first_5_bits}")
-    # print(f"Length: {length}")
-    # print(f"Remaining bits: {remaining_bits}")    
-    # print(f"Reverse: {password}")
     return password
 
-def hash(data:str, num_bits=60):
+def hash(data:str, num_bits=40):
   full = hashlib.sha1(data.encode()).hexdigest()
   hexa_words = num_bits // 4
   return full[:hexa_words]
@@ -24,36 +18,76 @@ def hash(data:str, num_bits=60):
 def gen_table(width: int, deep: int) -> dict:
     table ={}
     while len(table) < deep:
-        passwd = str(random.randrange(99999999999999999999))
+        repeated = False
+        passwd = str(random.randint(99999,99999999999999999999))
         p = passwd
         for j in range(width):
             h = hash(p)
             if h in table:
+                repeated = True
                 break
             p = reverse(h)
-        table[hash(p)] = passwd
+        if repeated: 
+            continue
+        table[h] = passwd
     print("Table generated", len(table))
     return table
 
-passwd = "67325"
+
+def search_collision(table,width):
+    hi = h
+    found = False
+    for i in range(width):
+        if hi in table:
+            print(f"Colision encontrada en index {i}, buscando hash en linea...")
+            trace_collision(table, hi)
+            found = find_original_hash(table,hi)
+            if found:
+                break;
+        hi = hash(reverse(hi))
+    if not found:
+        # throw exception
+        raise Exception("Password not found")
+
+def find_original_hash(table, hi):
+    pwd = table[hi]
+    hp = hash(pwd)
+    while h != hp:
+        if hp == hi:
+            print(f"Linea incorrecta, seguimos buscando...")
+            return False
+        hp = hash(pwd)
+        pwd = reverse(hp)
+    print(f"HASH ENCONTRADO: {hp}")
+    print(f"CONSTRASEÑA EQUIVALENTE: {pwd}")
+    return True
+
+
+def trace_collision(table, hi):
+    hp=h
+    print("COLLISION TRACE")
+    print(f"Found hash: {hi}")
+    print(f"Found password: {table[hi]}")
+    print(f"Original passwd: {passwd} ➜")
+    print(f"Original hash: {hp} ➜")
+    while hp != hi:
+        pp = reverse(hp)
+        hp= hash(pp)
+        print(f"reverse ➜ {pp}")
+        print(f"hash ➜ {hp}")
+
+
+
+width= 1000
+deep = 1000000
+passwd = "321321321"
 h = hash(passwd)
+
 print(f"Password: {passwd}")
 print(f"Hash of password {h}")
-# print("Reverse of hash:", reverse(hash("1234")))
-# print("Reverse of hash:", reverse(hash("12345")))
-# print("Reverse of hash:", reverse(hash("123456")))
-# print("Reverse of hash:", reverse(hash("1234567")))
-# print("Reverse of hash:", reverse(hash("12345678")))
-# print("Reverse of hash:", reverse(hash("123456789")))
-# print("Reverse of hash:", reverse(hash("1234567890")))
-# print("Reverse of hash:", reverse(hash("12345678901")))
-# print("Reverse of hash:", reverse(hash("123456789012")))
-# print("Reverse of hash:", reverse(hash("1234567890123")))
 
-width= 100
-deep = 10000
 try :
-    raise Exception("Table not found")
+    # raise Exception("Table not found")
     f_table = open('table.txt').read()
     table = ast.literal_eval(f_table)
 except:
@@ -63,31 +97,22 @@ except:
     f_table.write(str(table))
     f_table.close()
     
-hi = h
-for i in range(width):
-    if hi in table:
-        print(f"Found hash: {hi}")
-        print(f"Found password: {table[hi]}")
-        break
-    hi = hash(reverse(hi))
-    
-print(f"Password index: {i}")
-if i == width:
-    # throw exception
-    raise Exception("Password not found")
 
-pwd = table[hi]
+
+search_collision(table,width)
+
+
+
+
 
 print(f"Password line: {pwd}")
 
-for j in range(width):
-    if h == hash(pwd):
-        print(f"The password is: {pwd}")
-        break
-    pwd = reverse(hash(pwd))
+print("SEARCH TRACE")
+
     
+print(j)
 print(f"The password is: {pwd}")
-print(f"The hash of the generated password is: {hash(pwd)}")
+print(f"The hash of the generated password is: {hp}")
 
 
 
