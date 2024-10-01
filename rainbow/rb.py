2d1511375da41b666424d9963510c63bbb9a35cd
Alex Arenas <106154424+al3xar@users.noreply.github.com>
2024-10-01 15:16:03 +0000

    feat: probabilidad de colision correcta

--- rainbow/rb.py
+++ rainbow/rb.py
@@ -2,10 +2,15 @@ import hashlib
 import random
 import ast
 
-def reverse(hash: str) -> str:
-    return f"{int(hash, 16) % 10000}"
+width= 100
+deep = 100000
+size = 10000000
 
-def hash(data:str, num_bits=40):
+
+def reverse(hash: str) -> str:
+    return f"{int(hash, 16) % size}"
+
+def hash(data:str, num_bits=50):
   full = hashlib.sha1(data.encode()).hexdigest()
   hexa_words = num_bits // 4
   return full[:hexa_words]
@@ -13,12 +18,15 @@ def hash(data:str, num_bits=40):
 def gen_table(width: int, deep: int) -> dict:
     table ={}
     while len(table) < deep:
-        passwd = f"{random.randint(0, 9999)}"
+        passwd = f"{random.randint(0, size)}"
         p = passwd
         for _ in range(width):
             h = hash(p)
             p = reverse(h)
-        table[h] = passwd
+        if h not in table:
+            table[h] = passwd
+        else:
+            table[hash(reverse(h))] = passwd
     print("Table generated", len(table))
     return table
 
@@ -27,13 +35,15 @@ def search_collision(table,width):
     found = False
     for i in range(width):
         if hi in table:
-            print(f"Colision encontrada en index {i}, buscando hash en linea...")
-            trace_collision(table, hi)
-            found = find_original_hash(table,hi)
+            # print(f"Colision encontrada en index {i}, buscando hash en linea...")
+            # trace_collision(table, hi)
+            found, hp, pwd = find_original_hash(table,hi)
             if found:
                 break;
         hi = hash(reverse(hi))
-    if not found:
+    if found:
+        return hp, pwd
+    else:
         raise Exception("Password not found")
 
 def find_original_hash(table, hi):
@@ -41,13 +51,13 @@ def find_original_hash(table, hi):
     hp = hash(pwd)
     while h != hp:
         if hp == hi:
-            print(f"Falso positivo, seguimos buscando...")
+            # print(f"Falso positivo, seguimos buscando...")
             return False
         hp = hash(pwd)
         pwd = reverse(hp)
-    print(f"HASH ENCONTRADO: {hp}")
-    print(f"CONSTRASEÑA EQUIVALENTE: {pwd}")
-    return True
+    # print(f"HASH ENCONTRADO: {hp}")
+    # print(f"CONSTRASEÑA EQUIVALENTE: {pwd}")
+    return True, hp, pwd
 
 def trace_collision(table, hi):
     hp=h
@@ -60,14 +70,6 @@ def trace_collision(table, hi):
         print(f" reverse: {pp}", end=" ➜ ")
         print(f" hash {hp}", end=" ➜ ")
 
-width= 10
-deep = 1000
-passwd = "4321"
-h = hash(passwd)
-
-print(f"Password: {passwd}")
-print(f"Hash of password {h}")
-
 try :
     f_table = open('table.txt').read()
     table = ast.literal_eval(f_table)
@@ -77,5 +79,25 @@ except:
     f_table = open('table.txt', 'w')
     f_table.write(str(table))
     f_table.close()
-    
-search_collision(table,width)
+
+test_passwd = [f"{random.randint(0, size)}" for _ in range(10000)]
+found_passwords = []
+failed_passwords = []
+for p in test_passwd:
+    h = hash(p)
+    try:
+        hp, pwd = search_collision(table,width)
+        found_passwords.append(pwd)
+    except:
+        failed_passwords.append(p)
+        continue
+
+print("Passwords found: ", found_passwords)
+# print("Passwords not found: ", failed_passwords)
+
+# Porcentaje de acierto
+print(f"Porcentaje de acierto: {len(found_passwords)/len(test_passwd)*100}%")
+
+# Porcentaje de fallo
+print(f"Porcentaje de fallo: {len(failed_passwords)/len(test_passwd)*100}%")
+
